7e1bec7acc57d9bae4e7f2161aab0909fcf3b014
Eric McWinNEr <mcwinnerwebdeveloper@gmail.com>
2022-03-23 02:57:31 +0100

    Added new icons

---
+++ src/icons/MoonIcon.vue
@@ -0,0 +1,62 @@
+<template>
+  <svg
+    :width="width"
+    :height="height"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M2.03009 12.42C2.39009 17.57 6.76009 21.76 11.9901 21.99C15.6801 22.15 18.9801 20.43 20.9601 17.72C21.7801 16.61 21.3401 15.87 19.9701 16.12C19.3001 16.24 18.6101 16.29 17.8901 16.26C13.0001 16.06 9.00009 11.97 8.98009 7.14002C8.97009 5.84002 9.24009 4.61002 9.73009 3.49002C10.2701 2.25002 9.62009 1.66002 8.37009 2.19002C4.41009 3.86002 1.70009 7.85002 2.03009 12.42Z"
+      :stroke="smartColor || stroke"
+      :stroke-width="strokeWidth"
+      :stroke-linecap="strokeLineCap"
+      :stroke-linejoin="strokeLineJoin"
+    />
+  </svg>
+</template>
+
+<script setup>
+defineProps({
+  smartColor: {
+    type: String,
+    default: "currentcolor",
+  },
+  fill: {
+    type: String,
+  },
+  stroke: {
+    type: String,
+  },
+  strokeWidth: {
+    type: String,
+    default: "2",
+  },
+  strokeLineCap: {
+    type: String,
+    default: "round",
+  },
+  strokeLineJoin: {
+    type: String,
+    default: "round",
+  },
+  width: {
+    type: String,
+    default: "24",
+  },
+  height: {
+    type: String,
+    default: "24",
+  },
+  fillRule: {
+    type: String,
+    default: "evenodd",
+  },
+  clipRule: {
+    type: String,
+    default: "evenodd",
+  },
+});
+</script>
+
+<style scoped></style>
