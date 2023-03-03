9b3ca8dbebf751ae1895ed24883bb34750f70eaf
Eric McWinNEr <mcwinnerwebdeveloper@gmail.com>
2023-03-03 13:35:20 +0100

    Updated dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,18 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials",],
-  framework: "@storybook/vue3",
-  core: {
-    builder: "@storybook/builder-vite"
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/vue3",
+  "core": {
+    "builder": "@storybook/builder-vite"
   },
-
-  async viteFinal(config, {
-    configType
-  }) {
-    config.base = ''; //  './' also works, but i like "assets/.." urls better than "./assets/..." urls.
-
-    return config;
+  "features": {
+    "storyStoreV7": true
   }
-
-};
+}
