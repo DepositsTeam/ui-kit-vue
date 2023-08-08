a00dc6ac32cf75bc33040e10452ab3c820c1bcfb
Eric McWinNEr <mcwinnerwebdeveloper@gmail.com>
2023-08-08 02:27:07 +0100

    Updated the UI kit with the custom color config component

---
+++ storybook-static/assets/DCardInputField.stories-21d658f2.js
@@ -0,0 +1,21 @@
+import{a6 as t,_ as l}from"./DWysiwyg-c25e4b65.js";import"./vue.esm-bundler-d253c880.js";import"./floating-ui.dom-c3a31ea6.js";import"./_commonjsHelpers-042e6b4d.js";const D={title:"Forms/Card Input Field",component:t,argTypes:{label:{control:{type:"text"},table:{defaultValue:"Credit or Debit Card"}},cardNo:{control:{type:"text"}},cardCvv:{control:{type:"text"}},cardExp:{control:{type:"text"}},errorMessage:{control:{type:"text"}},size:{control:{type:"select"},options:["small","medium","large","xlarge","huge","massive"],table:{defaultValue:"huge"}},hideScanIcon:{control:{type:"boolean"}}}},m=r=>({components:{DCreditCardInput:t},setup(){return{args:r}},data:()=>({cardNo:"",cardCVC:"",cardExp:""}),template:'<d-credit-card-input v-model:card-exp="cardExp" v-model:card-no="cardNo" v-model:card-cvv="cardCVC" v-bind="args" />'}),i=r=>({components:{DCreditCardInput:t,DarkModeProvider:l},data:()=>({cardNo:"",cardCVC:"",cardExp:""}),setup(){return{args:r}},template:`
+    <dark-mode-provider :dark-mode="true">
+    <d-credit-card-input v-model:card-exp="cardExp" v-model:card-no="args.cardNo" v-model:card-cvv="cardCVC" v-bind="args" />
+    </dark-mode-provider>
+    `}),u=()=>{const r=i.bind({});return r.decorators=[()=>({template:'<div style="padding: 3em; background: #121A26;"><story /></div>'})],r},e=m.bind({}),a=u();var d,o,c;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`args => ({
+  components: {
+    DCreditCardInput
+  },
+  setup() {
+    return {
+      args
+    };
+  },
+  data: () => ({
+    cardNo: "",
+    cardCVC: "",
+    cardExp: ""
+  }),
+  template: \`<d-credit-card-input v-model:card-exp="cardExp" v-model:card-no="cardNo" v-model:card-cvv="cardCVC" v-bind="args" />\`
+})`,...(c=(o=e.parameters)==null?void 0:o.docs)==null?void 0:c.source}}};var n,p,s;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:"DarkModeTemplateFactory()",...(s=(p=a.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};const y=["Default","DefaultDark"];export{e as Default,a as DefaultDark,y as __namedExportsOrder,D as default};
+//# sourceMappingURL=DCardInputField.stories-21d658f2.js.map
