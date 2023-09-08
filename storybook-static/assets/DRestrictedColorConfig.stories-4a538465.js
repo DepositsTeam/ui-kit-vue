01bb0507da8abd05c4c5386dd36ac70480c22876
Eric McWinNEr <mcwinnerwebdeveloper@gmail.com>
2023-09-08 12:49:50 +0100

    Built project and storybook

---
+++ storybook-static/assets/DRestrictedColorConfig.stories-4a538465.js
@@ -0,0 +1,42 @@
+import{r as i,J as t,g as m}from"./DWysiwyg-2e36ce58.js";import"./vue.esm-bundler-d253c880.js";import"./floating-ui.dom-c3a31ea6.js";import"./_commonjsHelpers-042e6b4d.js";const E={components:{DAutoLayout:i},title:"Forms/Restricted Color Config",component:t,argTypes:{label:{control:{type:"text"},default:"Color"},colors:{control:{type:"object"}}}},u=r=>({components:{DRestrictedColorConfig:t},data:()=>({value:""}),setup(){return{args:r}},template:`
+      <d-restricted-color-config v-model="value" v-bind="args"/>`}),o=u.bind({});o.args={colors:[{colorCode:"#0C9CCC",colorName:"Teal"},{colorCode:"#1A8B5A",colorName:"Green"},{colorCode:"#2A2E33",colorName:"Black"},{colorCode:"#971E31",colorName:"Red"},{colorCode:"#E1E7EC",colorName:"White"},{colorCode:"#E06104",colorName:"Orange"},{colorCode:"#0059AC",colorName:"Blue"},{colorCode:"#4D36C0",colorName:"Purple"}],label:"Color"};const C=r=>({components:{DRestrictedColorConfig:t,DText:m,DAutoLayout:i},data:()=>({value:"#2A2E33"}),setup(){return{args:r}},template:`
+    <d-auto-layout direction="vertical">
+      <d-restricted-color-config v-model="value" v-bind="args"/>
+      <d-text>{{value}}</d-text>
+    </d-auto-layout>
+`}),e=C.bind({});e.args={colors:[{colorCode:"#0C9CCC",colorName:"Teal"},{colorCode:"#1A8B5A",colorName:"Green"},{colorCode:"#2A2E33",colorName:"Black"},{colorCode:"#971E31",colorName:"Red"},{colorCode:"#E1E7EC",colorName:"White"},{colorCode:"#E06104",colorName:"Orange"},{colorCode:"#0059AC",colorName:"Blue"},{colorCode:"#4D36C0",colorName:"Purple"}],label:"Color"};var a,l,c;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`args => ({
+  components: {
+    DRestrictedColorConfig
+  },
+  data: () => ({
+    value: ""
+  }),
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`
+      <d-restricted-color-config v-model="value" v-bind="args"/>\`
+})`,...(c=(l=o.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};var n,d,s;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`args => ({
+  components: {
+    DRestrictedColorConfig,
+    DText,
+    DAutoLayout
+  },
+  data: () => ({
+    value: "#2A2E33"
+  }),
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`
+    <d-auto-layout direction="vertical">
+      <d-restricted-color-config v-model="value" v-bind="args"/>
+      <d-text>{{value}}</d-text>
+    </d-auto-layout>
+\`
+})`,...(s=(d=e.parameters)==null?void 0:d.docs)==null?void 0:s.source}}};const f=["Default","InitialValue"];export{o as Default,e as InitialValue,f as __namedExportsOrder,E as default};
+//# sourceMappingURL=DRestrictedColorConfig.stories-4a538465.js.map
--- storybook-static/assets/preview-32dbbcb6.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-5a177a56.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-07d408ae.js"),["./DocsRenderer-3PUGWF3O-07d408ae.js","./chunk-S4VUQJ4A-c99ddff0.js","./iframe-5a177a56.js","./index-d8ade8ea.js","./_commonjsHelpers-042e6b4d.js","./index-d475d2ea.js","./index-d37d4223.js","./index-6ba087f2.js","./floating-ui.dom-c3a31ea6.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-32dbbcb6.js.map
