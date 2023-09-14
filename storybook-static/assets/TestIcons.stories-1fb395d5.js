fefa4dde9660219ad761590ec4efb2bfb7c985ae
Eric McWinNEr <mcwinnerwebdeveloper@gmail.com>
2023-09-14 12:57:21 +0100

    Made minor updates

---
+++ storybook-static/assets/TestIcons.stories-1fb395d5.js
@@ -0,0 +1,79 @@
+import{ap as K,aq as N,ar as Q,as as V,at as W,B as X,au as Y,av as Z,aw as ee,ax as re,ay as ae}from"./DWysiwyg-666d51f7.js";import"./vue.esm-bundler-673e2372.js";import"./floating-ui.dom-c3a31ea6.js";import"./_commonjsHelpers-042e6b4d.js";const te={title:"Icons/Test Icons for Review",component:K,argTypes:{}},e=L=>({setup(){return{args:L}},template:'<component :is="args.icon" />'}),r=e.bind({});r.args={icon:K};const a=e.bind({});a.args={icon:N};const s=e.bind({});s.args={icon:Q};const n=e.bind({});n.args={icon:V};const o=e.bind({});o.args={icon:W};const c=e.bind({});c.args={icon:X};const t=e.bind({});t.args={icon:Y};const i=e.bind({});i.args={icon:Z};const p=e.bind({});p.args={icon:ee};const m=e.bind({});m.args={icon:re};const l=e.bind({});l.args={icon:ae};var d,g,u;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`args => ({
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`<component :is="args.icon" />\`
+})`,...(u=(g=r.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};var I,x,F;a.parameters={...a.parameters,docs:{...(I=a.parameters)==null?void 0:I.docs,source:{originalSource:`args => ({
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`<component :is="args.icon" />\`
+})`,...(F=(x=a.parameters)==null?void 0:x.docs)==null?void 0:F.source}}};var E,_,S;s.parameters={...s.parameters,docs:{...(E=s.parameters)==null?void 0:E.docs,source:{originalSource:`args => ({
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`<component :is="args.icon" />\`
+})`,...(S=(_=s.parameters)==null?void 0:_.docs)==null?void 0:S.source}}};var b,f,w;n.parameters={...n.parameters,docs:{...(b=n.parameters)==null?void 0:b.docs,source:{originalSource:`args => ({
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`<component :is="args.icon" />\`
+})`,...(w=(f=n.parameters)==null?void 0:f.docs)==null?void 0:w.source}}};var $,D,h;o.parameters={...o.parameters,docs:{...($=o.parameters)==null?void 0:$.docs,source:{originalSource:`args => ({
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`<component :is="args.icon" />\`
+})`,...(h=(D=o.parameters)==null?void 0:D.docs)==null?void 0:h.source}}};var A,B,C;c.parameters={...c.parameters,docs:{...(A=c.parameters)==null?void 0:A.docs,source:{originalSource:`args => ({
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`<component :is="args.icon" />\`
+})`,...(C=(B=c.parameters)==null?void 0:B.docs)==null?void 0:C.source}}};var v,y,P;t.parameters={...t.parameters,docs:{...(v=t.parameters)==null?void 0:v.docs,source:{originalSource:`args => ({
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`<component :is="args.icon" />\`
+})`,...(P=(y=t.parameters)==null?void 0:y.docs)==null?void 0:P.source}}};var T,k,M;i.parameters={...i.parameters,docs:{...(T=i.parameters)==null?void 0:T.docs,source:{originalSource:`args => ({
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`<component :is="args.icon" />\`
+})`,...(M=(k=i.parameters)==null?void 0:k.docs)==null?void 0:M.source}}};var U,q,O;p.parameters={...p.parameters,docs:{...(U=p.parameters)==null?void 0:U.docs,source:{originalSource:`args => ({
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`<component :is="args.icon" />\`
+})`,...(O=(q=p.parameters)==null?void 0:q.docs)==null?void 0:O.source}}};var R,j,z;m.parameters={...m.parameters,docs:{...(R=m.parameters)==null?void 0:R.docs,source:{originalSource:`args => ({
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`<component :is="args.icon" />\`
+})`,...(z=(j=m.parameters)==null?void 0:j.docs)==null?void 0:z.source}}};var G,H,J;l.parameters={...l.parameters,docs:{...(G=l.parameters)==null?void 0:G.docs,source:{originalSource:`args => ({
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`<component :is="args.icon" />\`
+})`,...(J=(H=l.parameters)==null?void 0:H.docs)==null?void 0:J.source}}};const ie=["Dashboard2FilledIconExample","ActivityFilledIconExample","MessageFilledIconExample","BankFilledIconExample","CardFilledIconExample","Profile2UserFilledIconExample","BoxFilledIconExample","PersonalCardFilledIconExample","ArrowDownFilledIconExample","ShopFilledIconExample","SettingFilledIconExample"];export{a as ActivityFilledIconExample,p as ArrowDownFilledIconExample,n as BankFilledIconExample,t as BoxFilledIconExample,o as CardFilledIconExample,r as Dashboard2FilledIconExample,s as MessageFilledIconExample,i as PersonalCardFilledIconExample,c as Profile2UserFilledIconExample,l as SettingFilledIconExample,m as ShopFilledIconExample,ie as __namedExportsOrder,te as default};
+//# sourceMappingURL=TestIcons.stories-1fb395d5.js.map
---
+++ storybook-static/assets/preview-0b28368d.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-2ed87604.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-9ab0daa7.js"),["./DocsRenderer-3PUGWF3O-9ab0daa7.js","./chunk-S4VUQJ4A-f607d89d.js","./iframe-2ed87604.js","./index-d8ade8ea.js","./_commonjsHelpers-042e6b4d.js","./index-d475d2ea.js","./index-d37d4223.js","./index-6ba087f2.js","./floating-ui.dom-c3a31ea6.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-0b28368d.js.map
