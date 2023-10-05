801d73eb2fa6d966a9dd85957c7cc54df733c347
Eric McWinNEr <mcwinnerwebdeveloper@gmail.com>
2023-10-05 22:16:06 +0100

    Bumped up version

---
+++ storybook-static/assets/preview-b5ebdf5b.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-94a9beaf.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-baa93929.js"),["./DocsRenderer-3PUGWF3O-baa93929.js","./chunk-S4VUQJ4A-2ca5786d.js","./iframe-94a9beaf.js","./index-d8ade8ea.js","./_commonjsHelpers-042e6b4d.js","./index-d475d2ea.js","./index-d37d4223.js","./index-6ba087f2.js","./floating-ui.dom-c3a31ea6.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-b5ebdf5b.js.map
