2de3a606d88cb7fa986f1cb26414c8df3773625b
Eric McWinNEr <mcwinnerwebdeveloper@gmail.com>
2023-10-09 06:28:58 +0100

    Updated states list for countries dropdown

---
+++ storybook-static/assets/DActionableAvatar2.stories-124c6932.js
@@ -0,0 +1,14 @@
+import{e as f,j as v,k as a,u as e,l as g,q as t,m as s}from"./vue.esm-bundler-f842f4aa.js";import{O as b,h as o,c,g as r,ak as x,an as y}from"./DWysiwyg-7ab43336.js";import"./floating-ui.dom-c3a31ea6.js";import"./_commonjsHelpers-042e6b4d.js";const m={__name:"DActionableAvatar2",props:{label:{type:String},text:{type:String,required:!0},helperText:{type:String},size:{type:String}},setup(l){const _=f([{name:"John"},{name:"Nelly"},{name:"Saviour"},{name:"June"},{name:"Dave"},{name:"Nora"},{name:"Presh"},{name:"Master"}]);return(D,A)=>(g(),v(e(r),{class:"action__card"},{default:a(()=>[t(e(r),{class:"header"},{default:a(()=>[t(e(o),{class:"title","font-face":"circularSTD"},{default:a(()=>[s("Unpaid Invoices")]),_:1}),t(e(c),{"color-scheme":"outline",size:"large"},{default:a(()=>[s("See all")]),_:1})]),_:1}),t(e(r),{class:"body"},{default:a(()=>[t(e(r),{class:"left"},{default:a(()=>[t(e(o),{class:"title","font-face":"circularSTD"},{default:a(()=>[s("Overdue")]),_:1}),t(e(o),{class:"text","font-face":"circularSTD"},{default:a(()=>[s("16 ($41,350.56)")]),_:1})]),_:1}),t(e(r),{class:"right"},{default:a(()=>[t(e(x),{subtle:!0,size:"huge",avatars:_.value,stacked:!0,"visible-avatars":4},null,8,["avatars"])]),_:1})]),_:1}),t(e(r),{class:"footer"},{default:a(()=>[t(e(c),{"color-scheme":"primary",size:"large"},{rightIcon:a(()=>[t(e(r),{class:"right__icon"},{default:a(()=>[t(e(y))]),_:1})]),default:a(()=>[s(" Send reminder ")]),_:1})]),_:1})]),_:1}))}},d=b(m,[["__scopeId","data-v-04c532af"]]);m.__docgenInfo={exportName:"default",displayName:"DActionableAvatar2",description:"",tags:{},props:[{name:"label",type:{name:"string"}},{name:"text",type:{name:"string"},required:!0},{name:"helperText",type:{name:"string"}},{name:"size",type:{name:"string"}}],sourceFiles:["/Users/ericmcwinner/Documents/Work/Projects/Work/Vue/deposits-ui-kit-vue/src/d-actionable-avatar2/DActionableAvatar2.vue"]};const w={title:"Actionable Avatar 2",component:d,argTypes:{label:{control:{type:"text"}},text:{control:{type:"text"}},helperText:{control:{type:"text"}},size:{control:{type:"select"},options:["small","medium","large","xlarge","huge","massive"]}}},h=l=>({components:{DActionableAvatar2:d},setup(){return{args:l}},template:`
+    <d-actionable-avatar2 v-bind="args"/>`}),n=h.bind({});n.args={label:"Demo Key",text:"dk_17j48r8qnxq9984w9429uqcwur09quw0r",helperText:"**Use this key in public places such as our JS web sdk."};var i,u,p;n.parameters={...n.parameters,docs:{...(i=n.parameters)==null?void 0:i.docs,source:{originalSource:`args => ({
+  components: {
+    DActionableAvatar2
+  },
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`
+    <d-actionable-avatar2 v-bind="args"/>\`
+})`,...(p=(u=n.parameters)==null?void 0:u.docs)==null?void 0:p.source}}};const z=["Default"];export{n as Default,z as __namedExportsOrder,w as default};
+//# sourceMappingURL=DActionableAvatar2.stories-124c6932.js.map
