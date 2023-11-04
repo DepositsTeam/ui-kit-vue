cc1feb4a7f7b3640561c88bf560ecb1bcb18fbc6
Eric McWinNEr <mcwinnerwebdeveloper@gmail.com>
2023-11-04 04:43:50 +0100

    Updated file-picker layout/ui

---
+++ storybook-static/assets/AutoLayout.stories-11bc2f87.js
@@ -0,0 +1,67 @@
+import{y as b,c as g}from"./DWysiwyg-10565f11.js";import"./vue.esm-bundler-64d654d1.js";import"./floating-ui.dom-c3a31ea6.js";import"./_commonjsHelpers-87174ba5.js";const m={title:"Layouts/Auto layout",component:b,tags:["autodocs"],argTypes:{direction:{control:{type:"select"},options:["horizontal","vertical"]},itemSpacing:{control:{type:"text"}},alignment:{control:{type:"select"},options:["top-left","top-center","top-right","center-right","center","center-left","bottom-left","bottom-center","bottom-right"]}}},d=c=>({components:{DAutoLayout:b,DBox:g},setup(){return{args:c}},template:`<d-auto-layout :switch-breakpoint="500" v-bind="args">
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" /> 
+  </d-auto-layout>`}),t=d.bind({}),r=d.bind({});r.args={switchBreakpoint:1e3};const o=d.bind({});o.args={switchWidth:300};var e,a,n;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`args => ({
+  components: {
+    DAutoLayout,
+    DBox
+  },
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`<d-auto-layout :switch-breakpoint="500" v-bind="args">
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" /> 
+  </d-auto-layout>\`
+})`,...(n=(a=t.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};var i,p,x;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`args => ({
+  components: {
+    DAutoLayout,
+    DBox
+  },
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`<d-auto-layout :switch-breakpoint="500" v-bind="args">
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" /> 
+  </d-auto-layout>\`
+})`,...(x=(p=r.parameters)==null?void 0:p.docs)==null?void 0:x.source}}};var u,s,h;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`args => ({
+  components: {
+    DAutoLayout,
+    DBox
+  },
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`<d-auto-layout :switch-breakpoint="500" v-bind="args">
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" />
+    <d-box background="#0CB9E9" border-radius="6px" height="48px" width="48px" /> 
+  </d-auto-layout>\`
+})`,...(h=(s=o.parameters)==null?void 0:s.docs)==null?void 0:h.source}}};const E=["Default","SwitchBreakpointHorizontal","SwitchWidthHorizontal"];export{t as Default,r as SwitchBreakpointHorizontal,o as SwitchWidthHorizontal,E as __namedExportsOrder,m as default};
+//# sourceMappingURL=AutoLayout.stories-11bc2f87.js.map
