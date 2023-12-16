6a97c67e44b842d7c0a89d503b8aa562b980f450
Eric McWinNEr <mcwinnerwebdeveloper@gmail.com>
2023-12-16 23:40:29 +0100

    Removed borders and card from empty state component

--- src/d-empty-state/DEmptyState.vue
+++ src/d-empty-state/DEmptyState.vue
@@ -1,62 +1,59 @@
 <template>
-  <d-card>
+  <d-box
+    border-radius="10px"
+    display="flex"
+    flex-direction="column"
+    justify-content="center"
+    align-items="center"
+    gap="30px"
+    padding-y="3rem"
+  >
     <d-box
-      border="1px dashed #E1E7EC"
-      border-radius="10px"
-      display="flex"
-      flex-direction="column"
-      justify-content="center"
-      align-items="center"
-      gap="30px"
-      padding-y="3rem"
+      border-radius="50%"
+      padding="2rem"
+      background-color="#F5F8FA"
+      display="inline-block"
     >
-      <d-box
-        border-radius="50%"
-        padding="2rem"
-        background-color="#F5F8FA"
-        display="inline-block"
-      >
-        <slot name="illustration">
-          <d-box is="img" :src="illustration" :alt="title" />
-        </slot>
-      </d-box>
-      <d-box text-align="center">
-        <slot name="title">
-          <d-heading
-            v-if="title"
-            scale="h5"
-            margin-bottom="0.5rem"
-            margin-top="0.5rem"
-            class="text-gray-700"
-            font-face="heroNew"
-            >{{ title }}
-          </d-heading>
-        </slot>
-        <slot name="description">
-          <d-text font-face="circularSTD" class="text-gray-600" center is="p-18"
-            >{{ description }}
-          </d-text>
-        </slot>
-      </d-box>
-      <d-box class="program_head_row-btn-cont">
-        <slot name="button">
-          <d-button
-            v-if="buttonText"
-            :size="buttonSize"
-            :smart-color="buttonSmartColor"
-            :color-scheme="buttonColorScheme"
-            @click="emit('clicked')"
-            responsive
-            >{{ buttonText }}
-          </d-button>
-        </slot>
-      </d-box>
+      <slot name="illustration">
+        <d-box is="img" :src="illustration" :alt="title" />
+      </slot>
     </d-box>
-  </d-card>
+    <d-box text-align="center">
+      <slot name="title">
+        <d-heading
+          v-if="title"
+          scale="h5"
+          margin-bottom="0.5rem"
+          margin-top="0.5rem"
+          class="text-gray-700"
+          font-face="heroNew"
+          >{{ title }}
+        </d-heading>
+      </slot>
+      <slot name="description">
+        <d-text font-face="circularSTD" class="text-gray-600" center is="p-18"
+          >{{ description }}
+        </d-text>
+      </slot>
+    </d-box>
+    <d-auto-layout class="program_head_row-btn-cont">
+      <slot name="button">
+        <d-button
+          v-if="buttonText"
+          :size="buttonSize"
+          :smart-color="buttonSmartColor"
+          :color-scheme="buttonColorScheme"
+          @click="emit('clicked')"
+          responsive
+          >{{ buttonText }}
+        </d-button>
+      </slot>
+    </d-auto-layout>
+  </d-box>
 </template>
 
 <script setup>
-import { DBox, DText, DCard, DButton, DHeading } from "@/main";
+import { DBox, DButton, DHeading, DText, DAutoLayout } from "@/main";
 
 defineProps({
   illustration: {
@@ -70,6 +67,7 @@ defineProps({
   },
   buttonText: {
     type: String,
+    default: "large",
   },
   buttonSize: {
     type: String,
---
+++ storybook-static/assets/DCopy.stories-dbf78d17.js
@@ -0,0 +1,12 @@
+import{am as s}from"./DWysiwyg-7adf6eb2.js";import"./vue.esm-bundler-41d8d808.js";import"./floating-ui.dom-c3a31ea6.js";import"./_commonjsHelpers-042e6b4d.js";const i={title:"Copy",component:s,tags:["autodocs"],argTypes:{}},a=n=>({components:{DCopy:s},setup(){return{args:n}},template:'<d-copy v-bind="args" />'}),t=a.bind({});var e,o,r;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`args => ({
+  components: {
+    DCopy
+  },
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`<d-copy v-bind="args" />\`
+})`,...(r=(o=t.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,i as default};
+//# sourceMappingURL=DCopy.stories-dbf78d17.js.map
