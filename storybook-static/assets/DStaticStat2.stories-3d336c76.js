1ffe72f7fee68932d6c5e3b58d92a578ee8543ef
Eric McWinNEr <mcwinnerwebdeveloper@gmail.com>
2024-01-24 18:14:59 +0100

    Publishing latest changes to npm

---
+++ storybook-static/assets/DStaticStat2.stories-3d336c76.js
@@ -0,0 +1,14 @@
+import{X as r,Y as s}from"./DWysiwyg-9f953ffe.js";import"./vue.esm-bundler-41d8d808.js";import"./floating-ui.dom-c3a31ea6.js";import"./_commonjsHelpers-042e6b4d.js";const d={title:"Static Stats 2",component:r,tags:["autodocs"],argTypes:{label:{control:{type:"text"}},icon:{control:{type:"object"}},text:{control:{type:"text"}},variant:{control:{type:"select"},options:["filled","outline"]},smartColor:{control:{type:"color"}}}},c=n=>({components:{DStaticStat2:r},setup(){return{args:n}},template:`
+      <d-static-stat2 v-bind="args"/>`}),t=c.bind({});t.args={label:"Entities",text:"10,432",icon:s,colorScheme:"outline"};var e,o,a;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`args => ({
+  components: {
+    DStaticStat2
+  },
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`
+      <d-static-stat2 v-bind="args"/>\`
+})`,...(a=(o=t.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,d as default};
+//# sourceMappingURL=DStaticStat2.stories-3d336c76.js.map
