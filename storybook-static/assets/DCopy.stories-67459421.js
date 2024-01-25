3215e05434f5dc09742fff678d445268445624f8
Eric McWinNEr <mcwinnerwebdeveloper@gmail.com>
2024-01-25 19:38:31 +0100

    Update version in package.json to 0.0.576 and built storybook

---
+++ storybook-static/assets/DCopy.stories-67459421.js
@@ -0,0 +1,12 @@
+import{as as s}from"./DWysiwyg-c244d28d.js";import"./vue.esm-bundler-a99e1ba3.js";import"./floating-ui.dom-c3a31ea6.js";import"./_commonjsHelpers-042e6b4d.js";const i={title:"Copy",component:s,tags:["autodocs"],argTypes:{}},a=n=>({components:{DCopy:s},setup(){return{args:n}},template:'<d-copy v-bind="args" />'}),t=a.bind({});var e,o,r;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`args => ({
+  components: {
+    DCopy
+  },
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`<d-copy v-bind="args" />\`
+})`,...(r=(o=t.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,i as default};
+//# sourceMappingURL=DCopy.stories-67459421.js.map
