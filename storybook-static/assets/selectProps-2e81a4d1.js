c67ec70b017eae01081a131b6fc894f34bf1729c
Eric McWinNEr <mcwinnerwebdeveloper@gmail.com>
2024-01-29 20:43:11 +0100

    Bumped up version number and built storybook

---
+++ storybook-static/assets/selectProps-2e81a4d1.js
@@ -0,0 +1,2 @@
+const t={options:{control:{type:"object"}},optionTitle:{control:{type:"text"}},optionValue:{control:{type:"text"}},returnFullObject:{control:{type:"boolean"}},optionDescription:{control:{type:"text"}},optionIcon:{control:{type:"text"}}};export{t as s};
+//# sourceMappingURL=selectProps-2e81a4d1.js.map
