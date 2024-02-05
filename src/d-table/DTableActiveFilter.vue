8bf13fc0829c900dafada79a1511cba8e7110c13
Eric McWinNEr <mcwinnerwebdeveloper@gmail.com>
2024-02-05 12:09:20 +0100

    Refactor DBanner layout and enhance DTable interaction
    The DBanner layout was refactored to use DAutoLayout for optimal alignment of elements. A new feature was introduced to the DTable component to provide efficient tracking of user row selections. A fresh component, DTableActiveFilter.vue, was developed for managing active table filters while enhancing the table UI. Lastly, click event handlers have been improved in DTable.vue for robust row selection.

---
+++ src/d-table/DTableActiveFilter.vue
@@ -0,0 +1,103 @@
+<template>
+  <d-box
+    @click="emit('clicked')"
+    class="ui-table-active-filter"
+    cursor="pointer"
+  >
+    <slot>
+      <d-box display="flex" gap="4px">
+        <slot name="icon"> </slot>
+        <d-text my0 margin-x="8px" font-face="circularSTD" scale="p-16">
+          {{ title1 }}
+          <d-box
+            :font-face="fontFace"
+            is="span"
+            light-color="#64748B"
+            dark-color="#64748B"
+          >
+            {{ filter1 }}
+          </d-box>
+          {{ value1 }}
+          <d-box is="span" v-if="join">
+            <d-box
+              :font-face="fontFace"
+              is="span"
+              light-color="#64748B"
+              dark-color="#64748B"
+            >
+              {{ join }}
+            </d-box>
+
+            <d-box is="span">{{ ` ${title2} ` }}</d-box>
+            <d-box
+              :font-face="fontFace"
+              is="span"
+              light-color="#64748B"
+              dark-color="#64748B"
+            >
+              {{ filter2 }}
+            </d-box>
+            {{ value2 }}
+          </d-box>
+        </d-text>
+        <close-icon @click="emit('close')"> </close-icon>
+      </d-box>
+    </slot>
+  </d-box>
+</template>
+
+<script setup>
+import DBox from "@/d-box/DBox.vue";
+import DText from "@/d-text/DText.vue";
+import CloseIcon from "@/icons/CloseIcon.vue";
+
+const emit = defineEmits(["close", "clicked"]);
+
+defineProps({
+  title1: {
+    type: String,
+  },
+  fontFace: {
+    type: String,
+    default: "circularSTD",
+  },
+  filter1: {
+    type: String,
+  },
+  value1: {
+    type: String,
+  },
+  join: {
+    type: String,
+  },
+  title2: {
+    type: String,
+  },
+  filter2: {
+    type: String,
+  },
+  value2: {
+    type: String,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.ui-table-active-filter {
+  display: inline-flex;
+  padding: 12px 16px;
+  justify-content: space-between;
+  background: #f5f8fa;
+  margin-top: 8px;
+  margin-bottom: 8px;
+  cursor: pointer;
+  border-radius: 5px;
+  &.dark_mode {
+    background: #2a2e33;
+  }
+  &.active {
+    border: 1px solid var(--light-primary-color);
+    box-shadow: 0 0 0 3px rgba(67, 210, 250, 0.25);
+  }
+}
+</style>
---
+++ storybook-static/assets/DCopy.stories-177d5964.js
@@ -0,0 +1,12 @@
+import{av as s}from"./DWysiwyg-c59c259e.js";import"./vue.esm-bundler-a99e1ba3.js";import"./floating-ui.dom-c3a31ea6.js";import"./_commonjsHelpers-042e6b4d.js";const i={title:"Copy",component:s,tags:["autodocs"],argTypes:{}},a=n=>({components:{DCopy:s},setup(){return{args:n}},template:'<d-copy v-bind="args" />'}),t=a.bind({});var e,o,r;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`args => ({
+  components: {
+    DCopy
+  },
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`<d-copy v-bind="args" />\`
+})`,...(r=(o=t.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,i as default};
+//# sourceMappingURL=DCopy.stories-177d5964.js.map
